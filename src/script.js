9636378aea02ae65bee413bb0fed0ba849536a8c
phanison898 <phanison898@gmail.com>
2022-12-09 14:43:48 +0530

    added login form

--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
 console.log("hey there");
+
+function login() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  if (username == "") {
+    alert("Please enter username ");
+    return;
+  } else if (password == "") {
+    alert("Please enter password ");
+  } else {
+    alert("Hello, " + username);
+  }
+}
